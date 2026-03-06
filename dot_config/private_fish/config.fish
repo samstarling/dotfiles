c72145e1f7a6b9c13989dc0701f9bc1e6a09d94e
Sam Starling <42478+samstarling@users.noreply.github.com>
2026-03-06 03:50:00 +0000

    Fix Brewfile template, nicer colours for prompt

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,8 +10,9 @@ end
 
 set fish_greeting
 set -U hydro_symbol_prompt "›"
+set -U hydro_color_prompt 808080
 set -U hydro_fetch false
 set -U hydro_color_pwd 808080
 set -U hydro_color_git fab387
-set -U hydro_symbol_git_dirty "± "
+set -U hydro_symbol_git_dirty " ±"
 set -U hydro_cmd_duration_threshold 9999999
