7f3db8a5cab29b56fc677ea3b961f594fdbf0c01
Sam Starling <42478+samstarling@users.noreply.github.com>
2026-03-06 04:15:37 +0000

    Nicer greeting

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -11,7 +11,6 @@ end
 alias ls "eza --group-directories-first --git"
 alias ll "eza --group-directories-first --git -l"
 
-set fish_greeting
 set -U fish_color_no_newline --background=black black
 set -U hydro_symbol_prompt "›"
 set -U hydro_color_prompt 808080
---
+++ dot_config/private_fish/functions/fish_greeting.fish
@@ -0,0 +1,23 @@
+function fish_greeting
+    # Date
+    set -l today (date "+%a %b %-d")
+
+    # Local IP (en0 = wifi, en1 = ethernet)
+    set -l ip (ipconfig getifaddr en0 2>/dev/null)
+    if test -z "$ip"
+        set ip (ipconfig getifaddr en1 2>/dev/null)
+    end
+    if test -z "$ip"
+        set ip offline
+    end
+
+    # Battery
+    set -l batt_raw (pmset -g batt 2>/dev/null)
+    set -l batt_pct (string match -r '[0-9]+%' -- $batt_raw)[1]
+    set -l batt_str $batt_pct
+    if string match -q "*AC Power*" -- $batt_raw
+        set batt_str "$batt_pct +"
+    end
+
+    echo "$today  |  $ip  |  $batt_str"
+end
